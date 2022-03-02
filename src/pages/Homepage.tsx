ea165d737f76d5d23f5c863489944391eed20aaf
Ed de Almeida <edvaldoajunior@gmail.com>
2022-03-02 18:30:57 -0300

    Homepage updated

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -3,7 +3,7 @@ import UltimosMovimentos from "../components/UltimosMovimentos";
 
 const Homepage = () => (
   <div className="container-fluid homepage">
-    <div className="col-9 last-movements">
+    <div className="col-8 last-movements">
       <UltimosMovimentos />
     </div>
     <div className="col-3 current-position">
