c30b03f8278a760aecb79d456a77c873886cb79a
Ed de Almeida <edvaldoajunior@gmail.com>
2022-03-03 05:52:14 -0300

    FormatAsMoney updated

--- src/utils/FormatAsMoney.js
+++ src/utils/FormatAsMoney.js
@@ -1,8 +1,9 @@
 const formatAsMoney = (valor) => {
-  let text = valor.toString();
-  let svalor = text.replace(".",",");
-  svalor = "R$ " + svalor;
-  return svalor;
+  let parts = valor.split(".");
+  console.log(parts);
+  let centsn = parseInt(parts[1],10);
+  let cents = (centsn<10) ? "0" + parts[1] : parts[1];
+  return parts[0] + "," + parts[1];
 }
 
 export default formatAsMoney;
