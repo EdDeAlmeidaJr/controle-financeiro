bb75bd66780ca0b9a73e50dfedb40c3333ad5acb
Ed de Almeida <edvaldoajunior@gmail.com>
2022-03-03 08:46:09 -0300

    FormatAsMoney removed

--- src/utils/FormatAsMoney.js
+++
@@ -1,9 +0,0 @@
-const formatAsMoney = (valor) => {
-  let parts = valor.split(".");
-  console.log(parts);
-  let centsn = parseInt(parts[1],10);
-  let cents = (centsn<10) ? "0" + parts[1] : parts[1];
-  return parts[0] + "," + parts[1];
-}
-
-export default formatAsMoney;
