9184a551d1a6086f3a87ef913af587c4e737cc8c
Ed de Almeida <edvaldoajunior@gmail.com>
2022-03-04 12:03:00 -0300

    Utils fixed to ESLint standards

--- src/utils/DataInvertida.js
+++ src/utils/DataInvertida.js
@@ -1,12 +1,12 @@
 const dataInvertida = (ano, mes, dia) => {
-  let res = ano.toString();
-  res += "/";
-  res += (mes >= 10) ? "" : "0";
-  res += mes.toString();
-  res += "/";
-  res += (dia >= 10) ? "" : "0";
-  res += dia.toString();
-  return res;
-}
+	let res = ano.toString();
+	res += "/";
+	res += (mes >= 10) ? "" : "0";
+	res += mes.toString();
+	res += "/";
+	res += (dia >= 10) ? "" : "0";
+	res += dia.toString();
+	return res;
+};
 
 export default dataInvertida;
