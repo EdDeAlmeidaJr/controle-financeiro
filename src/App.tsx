01940c5cdc819ed8218c2caf3d5003f6ae2a179a
Ed de Almeida <edvaldoajunior@gmail.com>
2022-03-04 12:03:30 -0300

    index and App fixed to ESLint standards

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // pages
-import Balanco from './pages/Balanco';
-import Homepage from './pages/Homepage';
-import Movimento from './pages/Movimento';
-import Orcamento from './pages/Orcamento';
+import Balanco from "./pages/Balanco";
+import Homepage from "./pages/Homepage";
+import Movimento from "./pages/Movimento";
+import Orcamento from "./pages/Orcamento";
 
 // components
-import Rodape from './components/Rodape';
-import Cabecalho from './components/Cabecalho';
+import Rodape from "./components/Rodape";
+import Cabecalho from "./components/Cabecalho";
 
 const App = () => (
-  <div className="App">
-    <Router>
-      <Cabecalho />
-      <div className='app-body'>
-        <Routes>
-          <Route path='/' element={<Homepage />} />
-          <Route path='/movimento' element={<Movimento />} />
-          <Route path='/balanco' element={<Balanco />} />
-          <Route path='/orcamento' element={<Orcamento />} />
-        </Routes>
-      </div>
-      <Rodape />
-    </Router>
-  </div>
-)
+	<div className="App">
+		<Router>
+			<Cabecalho />
+			<div className="app-body">
+				<Routes>
+					<Route path="/" element={<Homepage />} />
+					<Route path="/movimento" element={<Movimento />} />
+					<Route path="/balanco" element={<Balanco />} />
+					<Route path="/orcamento" element={<Orcamento />} />
+				</Routes>
+			</div>
+			<Rodape />
+		</Router>
+	</div>
+);
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-import './index.scss';
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import "./index.scss";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
